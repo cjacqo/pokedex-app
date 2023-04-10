fac2a6761ec6847710d9a2820039127c8e1f634f
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-04-10 10:25:30 -0700

    Removed initial code that was used to test that the scripts.js file was properly linked to the index.html file, then created array of objects for a few pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-// alert('Hello, World!')
-
-let favoriteFood = 'Sushi'
-document.write(favoriteFood)
+const pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: [ 'grass', 'poison' ]
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: [ 'fire' ]
+  },
+  {
+    name: 'Machamp',
+    height: 1.6,
+    types: [ 'fighting' ]
+  }
+]
